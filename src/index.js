5c064c138de9ed0c4de397d0a9de005162a398a7
Bhargav R G <bhargavr445@gmail.com>
2025-04-25 19:19:58 -0400

    first commit1

---
+++ src/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = add;
+exports.subtract = subtract;
+exports.multiply = multiply;
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+console.log(add(10, 20));
